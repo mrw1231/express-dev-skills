ff06a415b9f0d68dc74d550c15adffff0f80c5fc
Michael White <michael@Michaels-MacBook-Pro.local>
2021-12-17 11:59:36 -0700

    Add new skill button

--- controllers/skills.js
+++ controllers/skills.js
@@ -3,6 +3,8 @@ const Skill = require('../models/skill');
 module.exports = {
     index,
     show,
+    new: newSkill,
+    create,
 };
   
 function index(req, res) {
@@ -15,4 +17,13 @@ function show(req, res) {
     res.render('skills/show', {
       skills: Skill.getOne(req.params.id),
     });
-  }
+};
+
+function newSkill(req, res) {
+    res.render('skills/new')
+};
+
+function create(req, res) {
+    console.log(req.body);
+    res.redirect('skills');
+};
--- routes/skills.js
+++ routes/skills.js
@@ -6,9 +6,13 @@ var skillsCtrl = require('../controllers/skills.js');
 
 // All actual paths begin with "/skills"
  
-// GET /todos
+// GET /skills
 router.get('/', skillsCtrl.index);
-// GET /todos/:id
+// GET /skills/new
+router.get('/new', skillsCtrl.new);
+// GET /skills/:id
 router.get('/:id', skillsCtrl.show);
+// POST /skills
+router.post('/', skillsCtrl.create);
 
 module.exports = router;
